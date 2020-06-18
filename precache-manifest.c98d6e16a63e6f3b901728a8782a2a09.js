9f699d560d7af3314be762484d81a209cdb66acd
jturnerwhite@gmail.com <jturnerwhite@gmail.com>
2020-06-18 14:14:49 -0400

    Updates

---
+++ precache-manifest.c98d6e16a63e6f3b901728a8782a2a09.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76a0ffec1cef6d32cb840a1cbd6bb47f",
+    "url": "/tsl-talent-calc/index.html"
+  },
+  {
+    "revision": "a098c0a56cdaf9118683",
+    "url": "/tsl-talent-calc/static/css/main.64507339.chunk.css"
+  },
+  {
+    "revision": "c59df875c124c5ed9e42",
+    "url": "/tsl-talent-calc/static/js/2.85544654.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/tsl-talent-calc/static/js/2.85544654.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a098c0a56cdaf9118683",
+    "url": "/tsl-talent-calc/static/js/main.ade1d209.chunk.js"
+  },
+  {
+    "revision": "f19843fc20ab4ec84e24",
+    "url": "/tsl-talent-calc/static/js/runtime-main.1f2bbc55.js"
+  },
+  {
+    "revision": "181336bd1012f7324a9a0fb4e132f799",
+    "url": "/tsl-talent-calc/static/media/talent-calc-bg.181336bd.png"
+  },
+  {
+    "revision": "8a1f5ce5d87d1b9772739500bc5d2b29",
+    "url": "/tsl-talent-calc/static/media/talent-icons-sprite.8a1f5ce5.png"
+  }
+]);
